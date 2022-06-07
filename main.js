5604699c24fbcf2100060371bef5b62e0df12866
Evaldo Júnior <41309801+underlinejunior@users.noreply.github.com>
2022-06-07 08:32:38 -0300

    Ajustes de responsividade

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 1000,
+  duration: 500,
   reset: true
 }).reveal(
-  `img .titulo,body .title, header .nav, #home,.helenomaia, #sobre .title,#sobre .img,#sobre .text,#sobre .modalidades-title,#sobre .modalidades,#sobre .fotos,#map iframe,#map article,#contato article,.footer .fa-facebook-f,.footer .fa-instagram, .footer .footer-logo`,
-  { interval: 700 }
+  `img .titulo,body .title, header .nav, #home,.helenomaia, #sobre .title,#sobre .img,#sobre .fotos,#map iframe,#map article,#contato article,.footer .fa-facebook-f,.footer .fa-instagram, .footer .footer-logo`,
+  { interval: 500 }
 )
